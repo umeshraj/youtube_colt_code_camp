1cde5e0b42c22675d1e254b9dbbc6f19c90be703
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-20 10:00:42 -0400

    Basic javascript

---
+++ 15_js_beginners/script.js
@@ -0,0 +1,29 @@
+// strings
+let name = "umesh";
+console.log(name.length);
+console.log(name.toUpperCase());
+
+console.log(name.indexOf("me"));
+console.log(name.slice(1, 3));
+
+// conditonals
+let score = 70;
+if (score > 90) {
+  console.log("You got an A");
+} else if (score > 80) {
+  console.log("You get a B");
+} else {
+  console.log("You did not get an F");
+}
+
+// coin toss
+if (Math.random() > 0.5) {
+  console.log("Heads");
+} else {
+  console.log("Tails");
+}
+
+// arrays
+let nums = [23, 45, -1, 10];
+let hens = ["ur", "sx", "as"];
+console.log(hens);
